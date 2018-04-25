d194e3d11a0ff652ce15d8635da8e4b1d6d0745f
Ximena Milagros Collantes Velasco <xmcollantes@atsistemas.com>
2018-04-25 11:10:36 +0200

    added all changes to put data into musicDataset.datos_spotify_test

--- src/main.py
+++ src/main.py
@@ -3,17 +3,16 @@
 import time
 import logging
 import os
-from os import path
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from pymongo import MongoClient
 from google.cloud.bigquery.client import Client
 
-logging.basicConfig(filename='../datos/logs/logs_D.log', filemode='w', level=logging.INFO)
+fecha = time.strftime("%Y%m%d_%H%M")
+logging.basicConfig(filename='../datos/logs/logs_'+fecha+'.log', filemode='w', level=logging.INFO)
 logging.info('Fecha y hora de INICIO: ' + time.strftime('%c'))
 
-fichero_entrada = open('../datos/artistas/Billboard_authors_D.txt', 'r', encoding='utf-8')
+fichero_entrada = open('../datos/artistas/Billboard_authors_A.txt', 'r', encoding='utf-8')
 artistas_fichero = fichero_entrada.readline()
 fichero_entrada.close()
 
@@ -29,8 +28,8 @@ client_credentials_manager = SpotifyClientCredentials(client_id='9d4ce57a9fd7445
                                                       client_secret='254f846eba8a4388840c99a11032ca14')
 spotipy_instance = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-client = MongoClient()
-db = client.Test  # Nombre de la BBDD es Test
+#client = MongoClient()
+#db = client.Test  # Nombre de la BBDD es Test
 
 for artista in lista_unica_artistas:
     if len(artista) > 0: # Puede haber nombres de artistas con una sola letra. Ejemplo: D
@@ -49,22 +48,27 @@ for artista in lista_unica_artistas:
             #artist_name=artista
             #obtenemos los albunes
             albums = spotipy_instance.artist_albums(artist_id, album_type='Album', limit=50)  # Albumes de cada artista
-            logging.info('-- Numero de albumes: ' + str(len(albums['items'])))
-
+            #albums=results['items']
+            #albums.extend(results['items'])
+            logging.info('-- Numero de albumes: ' + str(len(albums)))
             for iteratorAlbum in range(len(albums['items'])): # Itera los albumes del artista
+            #for album in albums:
                 album=albums['items'][iteratorAlbum]
                 album_id =album['id']
                 album_type=album['album_type']
-                album_genres=album['genres']
-                album_label=album['label']
+                #album_genres=album['genres']
+                album_genres=""
+                #album_label=album['label']
+                album_label=""
                 album_name=album['name']
-                album_popularity=album['popularity']
+                #album_popularity=album['popularity']
+                album_popularity=""
                 album_release_date=album['release_date']
                 album_realease_date_precision=album['release_date_precision']
-                logging.info('-- Album: ' + album_id)
+                logging.info('--- Album['+str(iteratorAlbum)+']: ' + album_name+"("+album_id+")")
                 #obtenemos las tracks del album
                 tracks = spotipy_instance.album_tracks(album_id, limit=50)  # tracks de cada album
-                logging.info('Numero de canciones: ' + str(len(tracks['items'])))
+                logging.info('----Numero de canciones: ' + str(len(tracks['items'])))
                 try:
                     for iteratorTrack in range(len(tracks['items'])): # Itera las canciones que contiene el album
                         track_id = tracks['items'][iteratorTrack]['id']
@@ -93,22 +97,21 @@ for artista in lista_unica_artistas:
                             features_time_signature = features[0]['time_signature']
                             features_valence = features[0]['valence']
                             # Fila a insertar en la tabla
-                            rows_to_insert = [(artist_followers,artist_genres,artist_id,artista,artist_popularity,album_type,album_genres,album_id,album_label,album_name,album_popularity,album_release_date,album_realease_date_precision,features_acousticness,features_analysis_url,features_danceability,features_duration_ms,features_energy,features_instrumentalness,features_key,features_liveness,features_loudness,features_mode,features_speechiness,features_tempo,features_time_signature,features_valence,track_avalible_markets,track_explicit,track_id,track_name,track_popularidad,track_number)]
-                            
+                            rows_to_insert = [(artista,artist_followers,str(artist_genres),artist_id,artist_popularity,album_type,album_genres,album_id,album_label,album_name,album_popularity,album_release_date,album_realease_date_precision,track_name,str(track_avalible_markets),str(track_explicit),track_id,track_popularidad,track_number,features_acousticness,features_analysis_url,features_danceability,features_duration_ms,features_energy,features_instrumentalness,features_key,features_liveness,features_loudness,features_mode,features_speechiness,features_tempo,features_time_signature,features_valence)]
                             # Autenticación
                             os.environ[
-                                'GOOGLE_APPLICATION_CREDENTIALS'] = path + 'MusicProjectTest-98a6983937ab.json'
+                                'GOOGLE_APPLICATION_CREDENTIALS'] = '../datos/MusicProjectTest-98a6983937ab.json'
                             # Conexion con la API de Big Query
                             big_query_client = Client()
                             # Se establece la BBDD
-                            dataset_ref = big_query_client.dataset('Test_2')
+                            dataset_ref = big_query_client.dataset('musicDataset')
                             # establecemos la tabla
-                            table_ref = dataset_ref.table('track_features')
+                            table_ref = dataset_ref.table('datos_spotify_test')
                             table = big_query_client.get_table(table_ref)
                             # Insertar datos en Big Query
                             errors = big_query_client.insert_rows(table, rows_to_insert)  # API request
                             for error in errors:
-                                logging.error('Big Query: ' + errors)
+                                logging.error('Big Query: ' + error)
                         except :
                             logging.error('-- Artista -> %s, album -> %s y cancion -> %s' %(artista, album_id, track_id))
                 except:
